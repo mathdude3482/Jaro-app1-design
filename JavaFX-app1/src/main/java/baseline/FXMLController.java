d9323afe784ee48be8dc754ea31572c3e47daf2a
mathdude3482 <orajsamej@gmail.com>
2021-10-24 17:43:32 -0400

    Updated UML diagram.

--- JavaFX-app1/src/main/java/baseline/FXMLController.java
+++ JavaFX-app1/src/main/java/baseline/FXMLController.java
@@ -46,22 +46,33 @@ public class FXMLController {
     @FXML
     private DatePicker pickDate3;
     @FXML
-    void buttonClicked(ActionEvent event) {
+   public void buttonClicked(ActionEvent event) {
         //if a button from the Menu of buttons is picked, call a method based on what is chosen.
-        //if the user wants to add a list,
+        //if the user wants to add a list, call another method called infoEntered that will
+        // display another screen.
+        //if the user wants to delete a list, call another method called infoDeleted that will
+        //display another screen.
     }
     @FXML
-    void chooseOption(ActionEvent event) {
+    public void chooseOption(ActionEvent event) {
+        //this method allows the user to choose an option from the MenuButton.
     }
     @FXML
-    void dateClicked(ActionEvent event) {
+   public void dateClicked(ActionEvent event) {
+        //if the user clicks a date, pop up another GUI that confirms the date he or she clicked.
     }
     @FXML
-    void infoEntered(ActionEvent event) {
+    public void infoEntered(ActionEvent event) {
+        //this method will pop up another GUI that allows the user to add information.
     }
 
     @FXML
-    void menuOptionClicked(ActionEvent event){
+   public void menuOptionClicked(ActionEvent event){
+        //based on what the user picks, this will pop up another GUI that will give the user
+        //what info they are looking for.
     }
-
+    @FXML
+   public void infoDeleted(ActionEvent event){
+        //this method will open another GUI that allows the user to delete a list.
+    }
 }
