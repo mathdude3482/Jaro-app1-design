7a87caa744236f6b0e740d8453db0e774c1c7d4d
mathdude3482 <orajsamej@gmail.com>
2021-10-24 21:50:05 -0400

    Updated PUML diagram, added a new class, and added skeleton for JUnit Testing.

---
+++ JavaFX-app1/src/main/java/baseline/Modify.java
@@ -0,0 +1,34 @@
+package baseline;
+
+import java.util.List;
+
+public class Modify {
+    //this class will contain methods that will modify a list.
+    public List<String> addList(){
+        //take in the name of a file.
+        //read in the contents of the file.
+        //store the contents into a List.
+        //return the list.
+        return null;
+    }
+    public List<String> deleteList(){
+        //take in the name of a file.
+        //read in the contents of the file.
+        //ask for a search string.
+        //if the search string is found, delete the list.
+        //return the list.
+        return null;
+    }
+    public List<String> modify(){
+        //take in the name of a file.
+        //read in the contents of the file.
+        //ask for a search string.
+        //if the search string is found, replace it with a new String that the user inputs.
+        //return the list.
+        return null;
+    }
+    public void writeToFile(){
+        //take in one parameter: an arrayList.
+        //write to an output file.
+    }
+}
---
+++ JavaFX-app1/src/test/java/baseline/ModifyTest.java
@@ -0,0 +1,28 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModifyTest {
+
+    @Test
+    void addList() {
+        //test if addList works.
+    }
+
+    @Test
+    void deleteList() {
+        //test if deleting a list works.
+    }
+
+    @Test
+    void modify() {
+        //test if modify works.
+    }
+
+    @Test
+    void writeToFile() {
+        //test if the output file can be found.
+    }
+}
